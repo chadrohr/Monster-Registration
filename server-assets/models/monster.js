d355c78ab57c6a72509e66fb2683679c8c872c2a
Chad Rohr <chadrohr@yahoo.com>
2016-10-18 13:26:19 -0600

    basic setup

---
+++ server-assets/models/monster.js
@@ -0,0 +1,40 @@
+let routes = require('express').Router();
+
+let monsters = [];
+
+function addMonster(monster) {
+    monsters.push(monster);
+}
+function removeMonster(index) {
+    monsters.splice(index, 1);
+}
+function editMonster(index, newMonster) {
+    if (index < monsters.length) {
+        todos[index] = newMonster
+        return { message: 'Successfully editted' }
+    }
+    return { error: 'Out of bounds' }
+}
+
+
+    routes.route('/monsters/:index?')
+    .get(function (req, res) {
+        res.send(monsters);
+
+    })
+    .post(function (req, res) {
+        addMonster(req.body.monster)
+        res.send({ message: 'Successfully added to monsters' })
+    })
+    .put(function (req, res) {
+        res.send(
+            editMonster(req.params.index, req.body.monster)
+        )
+    })
+    .delete(function (req, res) {
+        removeMonster(req.params.index)
+        res.send({ message: 'Successfully removed' })
+    })
+    module.exports = {
+        routes
+    }
